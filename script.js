e9e71dfa4dfa8897b95716a4d7829b25945e886f
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-02-14 19:59:15 +0100

    function to check who win the game

--- script.js
+++ script.js
@@ -1,56 +1,84 @@
 const gameBox = document.querySelector(".game");
 
-let squares = [];
-let squaresNum = 0;
-let idNumber = 0;
-let playerOne;
-let playerTwo;
-let computer;
-let activePlayer = 0;
-let squarebox;
-let isCycle = false;
-let isCross = false;
+const player_O = "O";
+const player_X = "X";
+let currentPlayer = player_O;
 
-const config = {
-  easy: {
-    squaresNum: 9,
-  },
-  medium: {
-    squaresNum: 25,
-  },
-  hard: {
-    squaresNum: 100,
-  },
-};
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let options = [null, null, null, null, null, null, null, null, null];
 
 const generateSquares = () => {
-  for (let square = 0; square < config.easy.squaresNum; square++) {
-    squarebox = document.createElement("button");
-    squarebox.classList.add("square");
-    squarebox.setAttribute("id", parseInt([square]) + 1);
-    squares.push(squarebox);
-    gameBox.appendChild(squarebox);
+  for (let square = 0; square < 9; square++) {
+    const squareCell = document.createElement("button");
+    squareCell.classList.add("square");
+    squareCell.setAttribute("id", parseInt([square] - 1) + 1);
+    gameBox.appendChild(squareCell);
+    console.log(square);
+    console.log(squareCell);
   }
 };
 
 generateSquares();
 
-// const toggleCycle = () => {
-//   isCycle = !isCycle;
+const squareBtn = document.querySelectorAll(".square");
 
-// };
+squareBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const id = e.target.id;
+    if (!options[id]) {
+      options[id] = currentPlayer;
+      e.target.innerText = currentPlayer;
+      console.log(options);
+      changePlayer();
+      if (whoWon(currentPlayer)) {
+        console.log(`${currentPlayer} wins`);
+      }
+    }
+  });
+});
 
-// // console.log(squares.length);
-// const squareBtn = document.querySelectorAll(".square");
-// squareBtn.forEach((btn) => {
-//   btn.addEventListener("click", () => {
-//     btn.textContent = "O";
-//     squarebox.classList.add("square-is-cycle");
-//     console.log(squarebox);
-//     console.log(btn);
-//     console.log(btn.id);
+const changePlayer = () => {
+  currentPlayer = currentPlayer === player_O ? player_X : player_O;
+};
 
-//     const random = Math.floor(Math.random() * squares.contains.length);
-//     squares[random].innerHTML = "X";
-//   });
-// });
+const whoWon = (player) => {
+  if (options[0] === player) {
+    if (options[1] === player && options[2] === player) {
+      return true;
+    }
+    if (options[3] === player && options[6] === player) {
+      return true;
+    }
+    if (options[4] === player && options[8] === player) {
+      return true;
+    }
+  }
+
+  if (options[8] === player) {
+    if (options[2] === player && options[5] === player) {
+      return true;
+    }
+    if (options[7] === player && options[6] === player) {
+      return true;
+    }
+  }
+
+  if (options[4] === player) {
+    if (options[3] === player && options[5] === player) {
+      return true;
+    }
+    if (options[1] === player && options[7] === player) {
+      return true;
+    }
+  }
+};
